793444a93a87c976eba2387c508ee89d0537c189
Seth Dorris <sethc.dorris@gmail.com>
2017-11-30 13:22:51 -0700

    fixing bot

--- bot.js
+++ bot.js
@@ -21,12 +21,9 @@ bot.on('message', message => {
     // Send "pong" to the same channel
       message.channel.send('pong');
     }
-    if (message.content === "stats") {
-        message.channel.send('What is your steam id? Remember to respond with the !sid prefix');
-    }
-    if (message.content.substring(0, 4) === "!sid") {
+    if (message.content.substring(0, 6) === "!stats") {
         console.log("Stats Firing");
-        var sid = message.content.substring(5);
+        var sid = message.content.substring(7);
         console.log("SID : ", sid);
         try {
             stats.getStats(sid,
@@ -46,9 +43,9 @@ function buildEmbed(data, author) {
     console.log("Player Stats", data.playerstats.stats);
     var kills = data.playerstats.stats[0].value;
     var deaths = data.playerstats.stats[1].value;
-    var shots = data.playerstats.stats[43].value;
-    var hits = data.playerstats.stats[42].value;
-    var headshots = data.playerstats.stats[23].value;
+    var shots = data.playerstats.stats[42].value;
+    var hits = data.playerstats.stats[41].value;
+    var headshots = data.playerstats.stats[22].value;
     var kd = (kills / deaths).toFixed(2);
     var hours = data.playerstats.stats[2].value / 60 / 60;
     return {

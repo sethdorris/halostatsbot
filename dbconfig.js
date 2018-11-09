135289dc67a1ec25da4bcd4fd212a11ef391b00a
Seth Dorris <sethc.dorris@gmail.com>
2018-11-09 08:19:26 -0700

    changing config

--- dbconfig.js
+++ dbconfig.js
@@ -1,15 +1,15 @@
 ﻿exports.production = {
-    user: "postgres",
-    password: "Seth42276",
-    database: "halodraftleague",
-    host: "localhost",
+    user: "ofgkdetmuojuzj",
+    password: "ec58355ec57c82450fe82daf69cdc83355e643ad2870bc1b0a2fdcc384519876",
+    database: "d7khijl3vv2fqr",
+    host: "ec2-204-236-230-19.compute-1.amazonaws.com",
     port: 5432
 }
 
 exports.development = {
-    user: "postgres",
-    password: "Seth42276",
-    database: "halodraftleague",
-    host: "localhost",
+    user: "ofgkdetmuojuzj",
+    password: "ec58355ec57c82450fe82daf69cdc83355e643ad2870bc1b0a2fdcc384519876",
+    database: "d7khijl3vv2fqr",
+    host: "ec2-204-236-230-19.compute-1.amazonaws.com",
     port: 5432
 }

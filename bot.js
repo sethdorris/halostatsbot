be50cf711c09eb17cd9954f6f2a6a0743e649007
Seth Dorris <sethc.dorris@gmail.com>
2018-11-16 09:05:56 -0700

    fix

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ bot.on('message', async message => {
     if (message.content.substring(0, 9) === "!register") {
         var role = message.member.guild.roles.find("name", "League Competitor");
         try {
-            message.author.addRole(role);
+            message.member.addRole(role);
             message.channel.send("You are now a registered competitor for the Halo Draft League!")
         } catch (e) {
             message.channel.send("Something went wrong, you were not successfully registered.")

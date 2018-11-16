9cd0ce0ea7f8f335f4e0fefd454e2d7aaef3dded
Seth Dorris <sethc.dorris@gmail.com>
2018-11-16 10:45:12 -0700

    change welcome message

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ bot.on("guildMemberAdd", async member => {
         var user = await pool.query(sql, [member.id]);
         var channel = member.guild.channels.find('name', 'general');
         if (user.rowCount < 1) {
-            channel.send(`Welcome to the Halo Draft League's Discord Channel ${member}. Please link your XBL gamertag by typing !linkgt followed by your gamertag. \`!linkgt itsme\``)
+            channel.send(`Welcome to the Halo Draft League's Discord Channel ${member}. Please link your XBL gamertag by typing !linkgt followed by your gamertag. \`!linkgt itsme\`. If you want to join the league, link your gamertag and then type \`!register\`.`)
         } else {
             channel.send(`Welcome back to the Halo Draft League. We found your gamertag: ${user.rows[0].gamertag}`)
             var role = member.guild.roles.find("name", "Linked");
